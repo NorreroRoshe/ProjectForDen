d3cef3924e196f1a7fe8a7450c83e1fe28228ab6
NorreroRoshe <norikas95@mail.ru>
2023-04-16 16:00:12 +0300

    ghps

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
-        <Route path="/ReactPizza/" element={<Home />} />
+        <Route path="/ProjectForDen/" element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/chapter" element={<Chapter />} />
         <Route
